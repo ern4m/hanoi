fcd49b91512b3e3eb558d4617ba71fef6e697831
ern4m <ernam.rsf@gmail.com>
2023-08-24 12:34:19 +0000

    Created structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+#[derive(Debug)]
+struct Block {
+    size: u8,
+} 
+
+impl Block {
+    fn smaller(&self, other: Block) -> bool {
+        return self.size < other.size
+    }
+}
+
+#[derive(Debug)]
+struct Stack {
+    items: Vec<Block>
+}
+
+impl Stack {
+    fn new() -> Stack {
+        Stack {
+            items: Vec::new()
+        }
+    }
+
+    fn from(vec: Vec<Block>) -> Stack {
+        Stack {
+            items: Vec::from(vec)
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Hanoi {
+    first_pile: Stack,
+    second_pile: Stack,
+    third_pile: Stack
+}
+
+impl Hanoi {
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let range = 1..9;
+
+    let size = range.len();
+
+    let mh = Hanoi {
+        first_pile: Stack::from(range.rev().into_iter().map(|i| Block { size: i }).collect()),
+        second_pile: Stack::new(),
+        third_pile: Stack::new()
+    };
+
+    dbg!("{:?}", mh);
 }
